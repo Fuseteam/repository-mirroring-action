7bb2b261aa45f7a9dc8668a63d5ffe872dc39b39
Rahammetoela Toekiman <fusekai@outlook.com>
2021-07-05 08:32:19 -0300

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,8 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+git config --global user.email $EMAIL
+git config --global user.name $NAME
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git pull --rebase mirror master
 git push mirror
