94eca25e40dff6d8c35134ddac5ea75838723cde
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-01 10:00:12 -0300

    set the git name and email from the last commit

--- mirror.sh
+++ mirror.sh
@@ -4,8 +4,10 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-git config --global user.email $INPUT_EMAIL
-git config --global user.name $INPUT_NAME
+EMAIL=`git log -1 --pretty=format:"%ae"`
+NAME=`git log -1 --pretty=format:"%an"`
+git config --global user.email "$EMAIL"
+git config --global user.name "$NAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git pull --rebase mirror "$INPUT_TARGET_BRANCH"
 git diff
